f35a13a6c5fffd1602c823ac842d11d158158ea9
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2022-03-23 22:04:05 -0300

    first commit

---
+++ PSO_POPULAÇÃO.py
@@ -0,0 +1,57 @@
+# https://en.wikipedia.org/wiki/Particle_swarm_optimization
+from copy import deepcopy
+import numpy as np
+
+MAX_GEN = 5
+TAM_POP = 10
+TAM_CROM = 2
+LI = -32.768
+LS = 32.768
+DIMEN = 2
+
+def GeraPopulacao(inf,sup):
+    return np.random.uniform(inf,sup, size=(TAM_POP, TAM_CROM))
+
+def Aptidao(cromossomo):
+    aux = -20*np.exp(-0.2*np.sqrt(0.5*(cromossomo[0]**2+cromossomo[1]**2))) \
+          - np.exp(0.5*(np.cos(2*np.pi*cromossomo[0])+np.cos(2*np.pi*cromossomo[1]))) + 20+ np.exp(1)
+    return aux
+
+pop = GeraPopulacao(LI,LS)
+melhor_local = deepcopy(pop[::])
+melhor_global = deepcopy(pop[0])
+velocidades = np.random.uniform(-1,1, size=(TAM_POP, TAM_CROM))
+
+for a in range(0, TAM_POP):
+    print(Aptidao(melhor_local[a]))
+    if Aptidao(melhor_local[a]) < Aptidao(melhor_global):
+        melhor_global = melhor_local[a]
+
+# Ate ate, ja temos inicializados: a)populacao b)melhor_local, c)melhor_global ed)velocidades
+
+i = 1
+w = 0.5
+c1 = 1
+c2 = 1
+while(i < MAX_GEN):
+    for b in range(0,TAM_POP):
+        for c in range(0,DIMEN):
+            r_p = np.random.uniform(0,1)
+            r_g = np.random.uniform(0,1)      
+            vel_cog = c1*r_p*(melhor_local[b,c] - pop[b,c])
+            vel_social = c2*r_g*(melhor_global[c] - pop[b,c])
+            velocidades[b,c] = w*velocidades[b,c] + vel_cog + vel_social
+            pop[b,c] = pop[b,c] + velocidades[b,c]
+            np.clip(pop[b,c],LI,LS)
+
+        if Aptidao(pop[b]) < Aptidao(melhor_local[b]):
+            melhor_local[b] = deepcopy(pop[b][:])
+            if Aptidao(melhor_local[b]) < Aptidao(melhor_global):
+                melhor_global = melhor_local[b][:]
+    i = i + 1
+    print(Aptidao(melhor_global))
+
+#print("Melhor de Todas as Gerações: " + str(melhor_global) + "\t Melhor Aptidão das últimas {} Gerações: ".format(MAX_GEN) + str(Aptidao(melhor_global)))
+
+
+
---
+++ ag_real.py
@@ -0,0 +1,71 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+MAX_GEN = 50
+TAM_POP = 100
+TAM_CROM = 2
+QT_TORNEIO = 3
+TX_CROSS = 0.7
+TX_MUT = 0.01
+LI = -32.768
+LS = 32.768
+
+def GeraPopulacao(inf,sup):
+    return np.random.uniform(inf,sup, size=(TAM_POP, TAM_CROM))
+
+def Aptidao(cromossomo):
+    aux = -20*np.exp(-0.2*np.sqrt(0.5*(cromossomo[0]**2+cromossomo[1]**2))) - np.exp(0.5*(np.cos(2*np.pi*cromossomo[0])+np.cos(2*np.pi*cromossomo[1]))) + 20+ np.exp(1)
+    return 1/(1.0+aux)
+    
+def CalculaAptidoes(pop):
+    return np.array([Aptidao(x) for x in pop])
+
+def SelecaoTorneio(aptidoes):
+    indices_cromo = np.random.randint(0,TAM_POP,size=QT_TORNEIO)
+    aptidoes_selecionadas = aptidoes[indices_cromo]
+    indice_melhor = np.argmax(aptidoes_selecionadas)
+    return indices_cromo[indice_melhor]    
+            
+def Cruzamento(pai,mae):
+    return ((pai+mae)/2.0)
+
+def Mutacao(cromossomo):
+    aux = cromossomo + np.random.normal(size=2)
+    return np.clip(aux,LI,LS)
+
+pop = GeraPopulacao(LI,LS)
+aptidoes = CalculaAptidoes(pop)
+
+medias = []
+melhores = []
+
+for g in range(MAX_GEN):
+    
+    nova_pop = []
+    for c in range(TAM_POP):
+        pai = pop[SelecaoTorneio(aptidoes)]
+        mae = pop[SelecaoTorneio(aptidoes)]
+        r = np.random.random()
+        if r <= TX_CROSS:
+            filho = Cruzamento(pai,mae)
+        else:
+            filho = pai[:]
+        r = np.random.random()
+        if r <= TX_MUT:
+            filho = Mutacao(filho)
+        r = np.random.random()            
+        nova_pop.append(filho)
+        
+    indice_melhor_pop = np.argmax(aptidoes)
+    aptidoes = CalculaAptidoes(nova_pop)
+    indice_pior_nova_pop = np.argmin(aptidoes)
+    nova_pop[indice_pior_nova_pop] = pop[indice_melhor_pop]
+    pop = nova_pop[:]
+    medias.append(np.mean(aptidoes))
+    melhores.append(np.max(aptidoes))
+    print ("Media: "+ str(medias[-1]) + "\t Melhor: "+str(melhores[-1]))
+    
+
+index_solucao = np.argmax(aptidoes)
+
+print ("Resposta final: " + str(pop[index_solucao]))
